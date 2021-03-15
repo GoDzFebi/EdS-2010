eac484b66d4cb76b1361b713e945b3523a3da5ec
Giulio Mazzetta <giulio.mazzetta.studenti@isii.com>
2021-03-15 16:29:40 +0100

    Modifica Homepage e CSS

---
+++ Index.php
@@ -0,0 +1,36 @@
+<?php
+//session.start()
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="CSS/Stylesheet.css" rel="stylesheet" type="text/css" />
+    <title>Supportish</title>
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+    <div class="container-fluid">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li><a id="len1" class="hoverable" href="#">Home</a></li>
+                    <li><a id="len2" class="hoverable" href="PHP - HTML/CronologiaTicket.php">I miei ticket</a></li>
+                    <li><a id="len3" class="hoverable" href="PHP - HTML/InserimentoTicket.php">Crea Ticket</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <footer class="footer">Copyright &copy;2020</footer>
+</body>
+
+</html>
